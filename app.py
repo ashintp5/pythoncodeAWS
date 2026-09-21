4d38dbf3e1f3c944602710fb54bacb0db2615809
ASHIN.T.P <ashin225603@sahrdaya.ac.in>
2026-09-21 17:10:36 +0530

    Adapt app for Hidden Places Explorer

--- app.py
+++ app.py
@@ -1,60 +1,75 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import boto3
 import pymysql
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-bucket_name="student-photo-demo-gopu"
+bucket_name = os.environ["S3_BUCKET_NAME"]
+db_host = os.environ["DB_HOST"]
+db_user = os.environ["DB_USER"]
+db_password = os.environ["DB_PASSWORD"]
+db_name = os.environ["DB_NAME"]
 
-db=pymysql.connect(
-host="100.57.165.48",
-port="3306",
-user="admin",
-password="Admin123",
-database="studentdb"
-)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/register',methods=['POST'])
+
+@app.route("/register", methods=["POST"])
 def register():
+    name = request.form["name"]
+    email = request.form["email"]
+    place_name = request.form["place_name"]
+    location = request.form["location"]
+    category = request.form["category"]
+    description = request.form["description"]
 
-    name=request.form['name']
-    email=request.form['email']
-    course=request.form['course']
+    photo = request.files["photo"]
+    filename = secure_filename(photo.filename)
 
-    photo=request.files['photo']
-
-    s3=boto3.client('s3')
+    s3 = boto3.client("s3")
 
     s3.upload_fileobj(
         photo,
         bucket_name,
-        photo.filename
+        filename,
+        ExtraArgs={"ContentType": photo.content_type}
     )
 
-    photo_url=f"https://{bucket_name}.s3.amazonaws.com/{photo.filename}"
+    photo_url = f"https://{bucket_name}.s3.eu-north-1.amazonaws.com/{filename}"
 
-    cursor=db.cursor()
+    db = pymysql.connect(
+        host=db_host,
+        port=3306,
+        user=db_user,
+        password=db_password,
+        database=db_name
+    )
 
-    sql="""
-    INSERT INTO students
-    (name,email,course,photo_url)
-    VALUES(%s,%s,%s,%s)
+    cursor = db.cursor()
+
+    sql = """
+    INSERT INTO places
+    (name, email, place_name, location, category, description, photo_url)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
     """
 
     cursor.execute(
         sql,
-        (name,email,course,photo_url)
+        (name, email, place_name, location, category, description, photo_url)
     )
 
     db.commit()
+    cursor.close()
+    db.close()
 
-    return "Student Registered Successfully"
+    return "Place submitted successfully!"
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=5000
